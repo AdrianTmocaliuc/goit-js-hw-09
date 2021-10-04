c41d83e94759bd1eb8cb676a469e831180c9afc5
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2021-10-04 19:17:46 +0300

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+import { getRandomHexColor } from './functions.js';
 
+
+const bodyStyle = document.querySelector('body');
+const startBUtton = document.querySelector('button')
+
+console.log(startBUtton.dataset.start)
+
+startBUtton.addEventListener('click', e => {
+    // e.target.dataset.start = bodyStyle.style.backgroundColor = 'black';
+    if (e.target.dataset.start) {
+        bodyStyle.style.backgroundColor = 'black';
+    };
+    if (e.target.dataset.stop) {
+        bodyStyle.style.backgroundColor = 'red';
+    };
+});
+// console.log(getRandomHexColor());
+bodyStyle.style.backgroundColor = 'black';
